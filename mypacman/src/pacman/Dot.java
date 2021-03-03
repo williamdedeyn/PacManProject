3026286d387620f0ba7c11c58badb9944c257d18
User <User@192.168.0.106>
2021-03-03 11:41:07 +0100

    implementatie dot

--- mypacman/src/pacman/Dot.java
+++ mypacman/src/pacman/Dot.java
@@ -5,10 +5,50 @@ package pacman;
  */
 public class Dot {
 	
+	/**
+	 * @invar | square != null
+	 * @invar | 0 <= square.getRowIndex() && 
+	 * 		  | square.getRowIndex() < square.getMazeMap().getHeight()
+	 * @invar | 0 <= square.getColumnIndex() && 
+	 * 		  | square.getColumnIndex() < square.getMazeMap().getWidth()
+	 * @invar | square.isPassable() == true
+	 */
+	/** @RepresentationObject */
 	private Square square;
-	
+
+	/**
+	 * @basic
+	 * 
+	 * @post | 0 <= result.getRowIndex() && 
+	 * 		 | result.getRowIndex() < result.getMazeMap().getHeight()
+	 * @post | 0 <= result.getColumnIndex() && 
+	 * 		 | result.getColumnIndex() < result.getMazeMap().getWidth()
+	 * @post | result.isPassable() == true
+	 * @post | result != null
+	 */
 	public Square getSquare() {return square;}
 	
-	public Dot(Square square) {this.square = square;}
+	/**
+	 * @throws IllegalArgumentException | square.isPassable() == false
+	 * @throws IllegalArgumentException | square == null
+	 * @throws IllegalArgumentException | 0 > square.getRowIndex() || 
+	 * 									| square.getRowIndex() >= square.getMazeMap().getHeight()
+	 * @throws IllegalArgumentException | 0 > square.getColumnIndex() || 
+	 * 									| square.getColumnIndex() >= square.getMazeMap().getWidth()
+	 * 
+	 * @inspects | square
+	 * 
+	 * @post | getSquare() == square
+	 */
+	public Dot(Square square) {
+		if(square == null)
+			throw new IllegalArgumentException("square can't be null");
+		if(0 > square.getRowIndex() || square.getRowIndex() >= square.getMazeMap().getHeight())
+			throw new IllegalArgumentException("Not a valid position");
+		if(0 > square.getColumnIndex() || square.getColumnIndex() >= square.getMazeMap().getWidth())
+			throw new IllegalArgumentException("Not a valid position");
+		if(square.isPassable() == false)
+			throw new IllegalArgumentException("Square is not free in the maze");
+		this.square = square;}
 
 }
