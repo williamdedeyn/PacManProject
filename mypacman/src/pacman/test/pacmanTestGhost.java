938e239d0ef12658a24bf6a7a1efa187a08c76c0
User <User@192.168.0.106>
2021-03-05 09:38:47 +0100

    verbeteringen

--- mypacman/src/pacman/test/pacmanTestGhost.java
+++ mypacman/src/pacman/test/pacmanTestGhost.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 
 import pacman.MazeMap;
-import pacman.PacMan;
 import pacman.Square;
 import pacman.Direction;
 import pacman.Ghost;
@@ -21,83 +20,85 @@ class pacmanTestGhost {
 		MazeMap mazemap = new MazeMap(2,2,new boolean[] {true, true, false, true});
 		
 		// Constructor Tests
-		
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { Ghost
-			  ghostInvalidSquare = new Ghost(null,Direction.RIGHT);});
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { Ghost
-			  ghostInvalidDirection = new Ghost(Square.of(mazemap, 1, 1),null);});
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { Ghost
-			  ghostInvalidSquare2 = new Ghost(Square.of(mazemap, 3, 0),Direction.RIGHT);});
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { Ghost
-			  ghostInvalidSquare3 = new Ghost(Square.of(mazemap, 0, 2),Direction.RIGHT);});
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { Ghost
-			  ghostInvalidSquare4 = new Ghost(Square.of(mazemap, -1, 0),Direction.RIGHT);});
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { Ghost
-			  ghostInvalidSquare5 = new Ghost(Square.of(mazemap, 0, -2),Direction.RIGHT);});
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { Ghost
-			  ghostInvalidSquare6 = new Ghost(Square.of(mazemap, 1, 0),Direction.RIGHT);});
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { Ghost
-		      ghostInvalidDirection2 = new Ghost(Square.of(mazemap, 1, 1),Direction.LEFT);});
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { Ghost
-			  ghostInvalidDirection3 = new Ghost(Square.of(mazemap, 1, 1),Direction.DOWN);});
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { Ghost
-			  ghostInvalidDirection4 = new Ghost(Square.of(mazemap, 1, 1),Direction.RIGHT);});
+
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(null,Direction.RIGHT);});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(Square.of(mazemap, 1, 1),null);});
 		 
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(Square.of(mazemap, 3, 0),Direction.RIGHT);});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(Square.of(mazemap, 0, 2),Direction.RIGHT);});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(Square.of(mazemap, -1, 0),Direction.RIGHT);});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(Square.of(mazemap, 0, -2),Direction.RIGHT);});
+		 
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(Square.of(mazemap, 1, 0),Direction.RIGHT);});
+		 
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(Square.of(mazemap, 1, 1),Direction.LEFT);});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(Square.of(mazemap, 1, 1),Direction.RIGHT);});
+		 
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(Square.of(mazemap, 0, 0),Direction.UP);});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> { new Ghost(Square.of(mazemap, 0, 0),Direction.DOWN);});
+		
 		 Square square = Square.of(mazemap, 0, 1);
-		 Ghost ghostValid = new Ghost(square,Direction.DOWN);
+		 Direction direction = Direction.DOWN;
+		 Ghost ghostValid = new Ghost(square,direction);
+		 
 		 // getters Tests
+		 Square result = ghostValid.getSquare();
 		 assertEquals(square,ghostValid.getSquare());
-		 assertEquals(Direction.DOWN,ghostValid.getDirection());
+		 assertTrue(result != null);
+		 assertTrue(0 <= result.getRowIndex());
+		 assertTrue(result.getRowIndex() < result.getMazeMap().getHeight());
+		 assertTrue(0 <= result.getColumnIndex());
+		 assertTrue(result.getColumnIndex() < result.getMazeMap().getWidth());
+		 assertTrue(result.isPassable() == true);
+		 assertEquals(direction,ghostValid.getDirection());
+		 assertTrue(ghostValid.getDirection() != null);
 		 
 		 //setSquare Test
 		 
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { 
-			 ghostValid.setSquare(Square.of(mazemap, 1, 0));
-		 });
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { 
-			 ghostValid.setSquare(null);
-		 });
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { 
-			 ghostValid.setSquare(Square.of(mazemap, -2, 0));
-		 });
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { 
-			 ghostValid.setSquare(Square.of(mazemap, 5, 0));
-		 });
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { 
-			 ghostValid.setSquare(Square.of(mazemap, 0, -2));
-		 });
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { 
-			 ghostValid.setSquare(Square.of(mazemap, 1, 2));
-		 });
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setSquare(Square.of(mazemap, 1, 0));});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setSquare(null);});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setSquare(Square.of(mazemap, -2, 0));});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setSquare(Square.of(mazemap, 5, 0));});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setSquare(Square.of(mazemap, 0, -2));});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setSquare(Square.of(mazemap, 1, 2));});
 		 
 		 square = Square.of(mazemap, 1, 1);
 		 ghostValid.setSquare(square);
 		 assertEquals(square,ghostValid.getSquare());
-		 assertEquals(Direction.DOWN,ghostValid.getDirection());
+		 assertEquals(direction,ghostValid.getDirection());
 		 
 		 // setDirections Tests
 		 
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { 
-			 ghostValid.setDirection(null);
-		 });
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { 
-			 ghostValid.setDirection(Direction.RIGHT);
-		 });
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { 
-			 ghostValid.setDirection(Direction.DOWN);
-		 });
-		 Assertions.assertThrows(IllegalArgumentException.class, () -> { 
-			 ghostValid.setDirection(Direction.LEFT);
-		 });
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setDirection(null);});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setDirection(Direction.RIGHT);});
+
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setDirection(Direction.LEFT);});
+		 
+		 square = Square.of(mazemap, 0, 1);
+		 ghostValid.setSquare(square);
+		 assertEquals(square,ghostValid.getSquare());
+		 assertEquals(Direction.DOWN,ghostValid.getDirection());
+		 ghostValid.setDirection(Direction.LEFT);
+		 assertEquals(Direction.LEFT,ghostValid.getDirection());
+		 ghostValid.setDirection(Direction.RIGHT);
+		 assertEquals(Direction.RIGHT,ghostValid.getDirection());
 		 ghostValid.setDirection(Direction.UP);
 		 assertEquals(Direction.UP,ghostValid.getDirection());
-		 assertEquals(square,ghostValid.getSquare());
 		 
+		 square = Square.of(mazemap, 0, 0);
+		 ghostValid.setDirection(Direction.LEFT);
+		 ghostValid.setSquare(square);
 		 
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setDirection(null);});
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setDirection(Direction.UP);});
+
+		 Assertions.assertThrows(IllegalArgumentException.class, () -> {ghostValid.setDirection(Direction.DOWN);});
 		 
-	
-	
-	
+		 ghostValid.setDirection(Direction.LEFT);
+		 assertEquals(Direction.LEFT,ghostValid.getDirection());
+		 ghostValid.setDirection(Direction.RIGHT);
+		 assertEquals(Direction.RIGHT,ghostValid.getDirection());
+		 
 	}
 
 }
